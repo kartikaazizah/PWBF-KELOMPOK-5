efe230af82b676d7ffdc6717017a356959f9bb14
kartikaazizah <134347941+kartikaazizah@users.noreply.github.com>
2023-07-20 22:58:26 +0700

    Add files via upload

---
+++ public/destinasi.js
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Objek Wisata</title>
+    
+   <link rel="stylesheet" href="css/objekwisata.css" />
+  </head>
+  <body>
+    <div class="grid">
+      <div class="grid-item">
+        <div class="card">
+          <div class="card-content">
+            <h1 class="card-header">Coban Lanang</h1>
+            <p class="card-text">
+             Destinasi ini mempunyai keindahan alam yang terdapat air terjun yang dapat kalian lihat. kalian juga bisa menikmati suasana alam yang ada disana dengan suara rintik-rintik air dan kalian dapat berfoto di beberapa spot menarik yang disediakan.
+            </p>
+          <a href="https://www.datawisata.com/chinatown-bandung" class="card-btn">Visit <span>&rarr;</span></a>
+          </div>
+        </div>
+      </div>
+      <div class="grid-item">
+        <div class="card">
+          <div class="card-content">
+            <h1 class="card-header">Telaga Madiredjo</h1>
+            <p class="card-text">
+               Wisata ini merupakan telaga yang terdapat cafe dan tempat duduk santai untuk menikmati pemandangan alam yang indah dan sejuk. Air di telaga ini juga jernih dan dapat berenang di telaga itu bersama ikan-ikan yang ada.
+            </p>
+            <a href="https://ciwideyvalley.com" class="card-btn">Visit <span>&rarr;</span></a>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="grid">
+      <div class="grid-item">
+        <div class="card">
+          <div class="card-content">
+            <h1 class="card-header">Coban Talun</h1>
+            <p class="card-text">
+             Wisata ini memiliki berbagai macam tempat, namun yang  utama yaitu air terjun nya yang indah. Selain air terjun, anda bisa mengujungi tempat lain yaitu camp apache, spot berfoto, taman gathering, hutan pinus dan beberapa spot wisata lainnya dengan keindaham alam yang asri.
+            </p>
+          <a href="https://www.datawisata.com/chinatown-bandung" class="card-btn">Visit <span>&rarr;</span></a>
+          </div>
+        </div>
+      </div>
+      </div>
+</body>
+</html>
